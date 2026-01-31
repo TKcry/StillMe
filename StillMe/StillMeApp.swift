a391cafb12de3b183cafd14ffe2a67810f2f8c81
Kosuke <kosukeq@yahoo.co.jp>
2026-02-01 02:31:14 +0900

    Initial Commit

---
+++ StillMe/StillMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StillMeApp.swift
+//  StillMe
+//
+//  Created by K on 2026/02/01.
+//
+
+import SwiftUI
+
+@main
+struct StillMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
